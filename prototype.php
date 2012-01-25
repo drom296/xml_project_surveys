e7541f76d500a009a09c29521c5446dae91feebe
Pedro Mass <drom296@gmail.com>
2012-01-24 21:16:41 -0500

    initial submission

---
+++ prototype.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>prototype</title>
+		<!-- CSS -->
+		<!-- CSS reset -->
+		<!-- 		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.4.1/build/cssreset/cssreset-min.css"> -->
+		<!-- 		<link rel="stylesheet" type="text/css" href="css/css_reset.css"> -->
+		<link rel="stylesheet" type="text/css" href="css/nav.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<!-- JavaScript -->
+	</head>
+	<body>
+		<div id="page">
+			<div id="header">
+				<!-- banner -->
+				<h1>Banner</h1>
+				<!-- menu -->
+				<div id="navDiv">
+					<ul id="nav">
+						<li>
+							<a href="">Home</a>
+						</li>
+						<li>
+							<a href="">Surveys</a>
+							<ul>
+								<li>
+									<a href="">Take a Survey</a>
+								</li>
+								<li>
+									<a href="">View Results</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">Admin</a>
+						</li>
+					</ul>
+					<!-- <ul id="nav">
+					<li>
+					<a>Home</a>
+					</li>
+					<li>
+					<a>Surveys</a>
+					<ul>
+					<li>
+					<a>Take a Survey</a>
+					</li>
+					<li>
+					<a>View Survey Results</a>
+					</li>
+					</ul>
+					</li>
+					<li>
+					<a>Admin</a>
+					<ul>
+					<li><a>Add Survey</a></li>
+					<li><a>Edit Survey</a></li>
+					<li><a>Delete Survey</a></li>
+					</ul>
+					</li>
+					</ul> -->
+					<!-- clear the float -->
+<!-- 					<p class="noFloat"></p> -->
+				</div>
+			</div>
+			<div id="content" class="noFloat">
+				<h1>Content</h1>
+			</div>
+			<div id="footer">
+				<!-- copyright info -->
+				<!-- links -->
+				<h1>Footer</h1>
+			</div>
+		</div>
+	</body>
+</html>
