29375a72ff45d26a770dac9f5189d9ba85f8740d
Pedro Mass <drom296@gmail.com>
2012-01-24 22:33:30 -0500

    Functionality: header, footer, get xml files as array

---
+++ Lib_survey.php
@@ -0,0 +1,77 @@
+<?php
+
+define("XML_PATH", "xml");
+
+function html_header($title = "Untitled", $styles = null, $scripts = null) {
+	$string = <<<END
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<title>$title</title>
+END;
+
+	if (is_array($styles)) {
+		foreach ($styles as $style) {
+			$string .= "<link type='text/css' rel='stylesheet' href='$style' />\n";
+		}
+	} else if (is_string($styles)) {
+		$string .= "<link type='text/css' rel='stylesheet' href='$styles' />\n";
+	}
+
+	if (is_array($scripts)) {
+		foreach ($scripts as $script) {
+			$string .= "<script type='text/javascript' src='$script'></script>\n";
+		}
+	} else if (is_string($scripts)) {
+		$string .= "<script type='text/javascript' src='$scripts'></script>\n";
+	}
+
+	$string .= <<<END
+</head>
+<body>\n
+	<div id="page">\n
+END;
+
+	return $string;
+}
+
+function html_footer($text = "") {
+	$string = <<<END
+		<p><em>$text</em></p>
+	</div> <!-- id=page -->
+</body>
+</html>
+END;
+
+	return $string;
+}
+
+function getSurveys() {
+	$result = array();
+
+	if ($handle = opendir(XML_PATH)) {
+		while (false !== ($entry = readdir($handle))) {
+			if ($entry != "." && $entry != "..") {
+				$result[] = $entry;
+			}
+		}
+		closedir($handle);
+	}
+
+	return $result;
+}
+
+function getXSLTProcessor($fileName){
+	
+}
+?>
+
+
+
+<?php
+
+//test
+	print_r(getSurveys());
+
+?>
