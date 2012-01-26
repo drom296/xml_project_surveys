83ebfc23f8b6b1ba977f4b05e2bae3134c950d29
Pedro Mass <drom296@gmail.com>
2012-01-25 22:29:55 -0500

    initial version of take_survey

---
+++ take_survey.php
@@ -0,0 +1,47 @@
+<?php
+require("LIB_survey.php");
+
+$styles = array("css/nav.css", "css/main.css");
+
+// create header tags
+$output = html_header("Choose a Survey", $styles);
+
+// HEADER Section *****************************************
+
+// start the header section
+$output .= startDiv("header");
+
+// create banner div
+$output .= addBanner();
+
+// create the nav
+$output .= addNav();
+
+// end the header section
+$output .= endDiv("header");
+
+// CONTENT Section *****************************************
+
+// start the content section
+$output .= startDiv("content", "roundBox");
+
+// create list of available to take
+$output .= addTakeSurveyLinks();
+
+// end the content section
+$output .= endDiv("content");
+
+// FOOTER Section *****************************************
+
+// start the footer section
+$output .= startDiv("footer");
+
+// end the footer section
+$output .= endDiv("footer");
+
+
+// create footer
+$output .= html_footer("");
+
+echo $output;
+?>
